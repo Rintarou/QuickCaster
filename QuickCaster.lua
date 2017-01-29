a5513b6089b5793ab6140c121e1880f2eca83dcb
Rintarou <merlin.barzilai@gmail.com>
2017-01-29 20:17:42 +0100

    multi working weirdly

--- QuickCaster.lua
+++ QuickCaster.lua
@@ -1,8 +1,76 @@
---
--- Created by IntelliJ IDEA.
--- User: runner
--- Date: 12/01/17
--- Time: 18:29
--- To change this template use File | Settings | File Templates.
+--- created on 12/01/2017 by Runner
 --
 
+local QuickCaster = {}
+
+--- the main game object which is used a a reference of what is allowed
+--
+QuickCaster.Game = {}
+
+function QuickCaster.Game:new()
+    setmetatable({}, Game)
+
+    return self
+end
+
+--- the player object is used, as there will usually be two players
+--
+QuickCaster.Player = { validinputs = {}, stack = {}, completion = 0, score = 0 }
+
+function QuickCaster.Player:new( chainsize , validinputs )
+    setmetatable({}, Player)
+    self.validinputs = validinputs
+    self.chainsize = chainsize
+    self.stack = {}
+    self.completion = 0
+    self.score = 0
+    --self.game = game
+
+    return self
+end
+
+--- the renew() function rerolls a stack of commands for the player.
+-- since there
+function QuickCaster.Player:renew()
+    local vi = self.validinputs
+
+    for i = 1, self.chainsize, 1
+    do
+        self.stack[i] = vi[math.random(1, #vi)]
+    end
+    self.completion = 0
+end
+
+--- the successcheck() function takes an input and processes it
+-- results in either the command being correct and points being awarded
+-- or a renew() of the player's stack
+function QuickCaster.Player:successcheck(input)
+    if self:validcommand(input)
+     then
+        if input == self.stack[self.completion+1]
+        then
+            self.completion = self.completion+1
+        else
+            self:renew()
+        end
+        if self.completion == #self.stack
+        then
+            self.score = self.score+1
+            self:renew()
+        end
+    end
+end
+
+--- the validcommand() function checks that an input is indeed in the set of
+-- possible commands. it avoids presses on weird buttons resulting in a stack reset
+function QuickCaster.Player:validcommand(input)
+    for _, v in pairs(self.validinputs) do
+        if v == input
+        then
+            return true
+        end
+    end
+    return false
+end
+
+return QuickCaster -- module end
--- main.lua
+++ main.lua
@@ -1,14 +1,30 @@
---[[ created on 08/01/2017 by Runner
- ]]
+--- created on 08/01/2017 by Runner
+--
 
--- Load some default values for our rectangle.
+local qc = require('QuickCaster')
+
+local game
+local player1, player2
+a = 1
+
 function love.load()
-    x, y, w, h = 20, 20, 60, 20
-    joysticks = love.joystick.getJoysticks()
-    io.write('joysticks: ', #joysticks, '\n')
-    for k, v in pairs(joysticks) do
-      io.write(v, "\n")
-    end
+    game = qc.Game.new()
+    player1 = qc.Player:new(4, {'up','down','left','right','a','b','x','y','l','r'})
+    player1:renew()
+
+    player2 = qc.Player:new(4, {'q','s','d','f','g'})
+    player2:renew()
+
+    print('player1.validinputs: ', player1.validinputs[1], '\n')
+
+    math.randomseed(os.time())
+    --x, y, w, h = 20, 20, 60, 20
+
+--    joysticks = love.joystick.getJoysticks()
+--    io.write('joysticks: ', #joysticks, '\n')
+--    for k, v in pairs(joysticks) do
+--      io.write(v, "\n")
+--    end
 end
 
 -- Increase the size of the rectangle every frame.
@@ -18,27 +34,48 @@ end
 
 -- Draw a coloured rectangle.
 function love.draw()
+    if a==1 then print('player1.stack: ', player1.stack[1], '\n'); a = a+1 end
+
     love.graphics.setColor(0, 100, 100)
-    love.graphics.rectangle("fill", x, y, w, h)
+    --love.graphics.rectangle("fill", x, y, w, h)
+
+    -- PlayerOne ----------------------------------------
+    -- buttons --
+    love.graphics.setColor(0, 100, 100)
+    for i=1, player1.chainsize, 1 do
+        if i > player1.completion
+        then
+            love.graphics.setColor(100, 0, 0)
+        end
+        love.graphics.print(player1.stack[i], i*50, 100)
+    end
+
+    -- Score
+
+    -- PlayerTwo ----------------------------------------
+    love.graphics.setColor(0, 100, 100)
+    for i=1, player2.chainsize, 1 do
+        if i > player2.completion
+        then
+            love.graphics.setColor(100, 0, 0)
+        end
+        love.graphics.print(player2.stack[i], 500+i*50, 100)
+    end
+    -- Score
+
 end
 
 function love.keypressed(key)
-    if key == "escape" then
-        love.event.quit()
-    end
 
-    if key == "up" then
-        y = y-1
-    elseif key == "down" then
-        y = y+1
-    end
+    player1:successcheck(key)
 
-    if key == "left" then
-        x = x-1
-
-    elseif key == "right" then
-        x = x+1
-    end
+--    if key == stack[completion+1] then
+--        completion = completion+1
+--    else
+--        completion = 0
+--    end
+--
+    if key == 'escape' then love.event.quit() end
 end
 
 --function love.joystickpressed(joystick,button)
@@ -50,4 +87,8 @@ end
 function love.joystickadded(joystick)
     joy = joystick
     io.write('ok')
+end
+
+function randominput()
+    return validinputs[math.random(1, #validinputs)]
 end
